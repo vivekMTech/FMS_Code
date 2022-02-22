aa6b300ff2465a33d40ff2552be60f2f8d813ebb
vivekMTech <vivek.parmar@marutitech.com>
2022-02-23 00:14:43 +0530

    files added.

---
+++ FMS_Code/main2.py
@@ -0,0 +1,63 @@
+import asyncio  # asyncio for concurrency files process.
+import random  # random for generating strings and length
+
+
+class WriteRandomString:
+    def __init__(self):
+        self.const_string = 'MARUTI'  # take constant keyword as 'MARUTI'
+
+    # generating random string
+    async def generate_string(self):
+        chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'  # using chars variable, we generate random string
+        string_len = random.randint(5, 20)  # generate random string len
+        seq = [''.join(random.choices(chars, k=string_len)),
+               self.const_string]  # sequence list with random and const string
+        return ''.join(random.choices(seq))  # generate string from sequence and return it
+
+    # write random strings in files
+    async def write_random_strings(self, file_name):
+        file_descriptor = open(file_name, "a+")  # file descriptor for file
+        gen_string = await self.generate_string()  # generate string using generate_string() Coroutine
+        file_descriptor.write(gen_string + "\n")  # write string in file
+        file_descriptor.close()
+
+
+class WriteCountedString(WriteRandomString):
+    def __init__(self):
+        WriteRandomString.__init__(self)
+        # super().__init__()
+        # self.const_string = 'MARUTI'  # take constant keyword as 'MARUTI'
+
+    # count 'MARUTI' keyword from files
+    async def count_keyword(self, file_name):
+        with open(file_name, 'r') as file:  # open file in read mode
+            file_content = file.read(-1)  # -1 means read whole file content
+            count = file_content.count(self.const_string)  # count 'MARUTI' keyword from content
+        return count
+
+    # write counts in counts.log
+    async def write_counts(self, file_name1, file_name2):
+        count_task_file1 = asyncio.create_task(self.count_keyword(file_name1))  # task is count from file1
+        count_task_file2 = asyncio.create_task(self.count_keyword(file_name2))  # task is count from file2
+        count_file1, count_file2 = await asyncio.gather(count_task_file1, count_task_file2)
+
+        file_descriptor = open("counts.log", "w+")  # open file descriptor in write mode
+        file_descriptor.write("{} -> {}\n".format(file_name1, count_file1))  # write count of file1 in 'count.log'
+        file_descriptor.write("{} -> {}\n".format(file_name2, count_file2))  # write count of file2 in 'count.log'
+        file_descriptor.close()
+
+
+# define flow of coroutines
+async def main():
+    write_random_obj = WriteRandomString()  # WriteRandomString object for accessing coroutines
+    write_count_obj = WriteCountedString()  # WriteCountedString object for accessing coroutines
+    while True:
+        write_file1 = asyncio.create_task(
+            write_random_obj.write_random_strings("file1.txt"))  # call write_random_strings() for file1
+        write_file2 = asyncio.create_task(
+            write_random_obj.write_random_strings("file2.txt"))  # call write_random_strings() for file2
+        write_count_log = asyncio.create_task(write_count_obj.write_counts("file1.txt", "file2.txt"))  # call write in count.log
+        await asyncio.gather(write_file1, write_file2, write_count_log, return_exceptions=True)
+
+
+asyncio.run(main())
---
+++ README.md
@@ -0,0 +1,13 @@
+# FMS
+File Monitoring System using Python
+
+# Assignment - 1
+Problem statement: File Monitoring system
+
+The goal for this task is to design and develop file monitoring system.
+Generate a random writer that writes pseudo strings (with at least 50% probability of generating “MARUTI” keyword. Note that we are strictly looking at "MARUTI" keyword only as a single value not the MARUTI keyword inside a random string) into two separate files at regular interval of time.
+It should have capability to monitor both the files and count the total number for occurrences for “MARUTI” keyword by each file and write output to "counts.log" file.
+
+Here, pseudo strings = random strings of given any number of length
+
+
